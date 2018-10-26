bb29a8faa52425b919ffd51abc7ed04af82a74da
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-10-26 14:25:46 +0200

    Added ssh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ grep -q dotfiles $HOME/.bashrc || echo "[ -r $HOME/.dotfiles/bash/bashrc ] && . 
 ln -f -v -s $HOME/.dotfiles/home/gitignore $HOME/.gitignore
 ln -f -v -s $HOME/.dotfiles/home/gitconfig $HOME/.gitconfig
 ln -f -v -s $HOME/.dotfiles/home/emacs $HOME/.emacs
+ln -f -v -s $HOME/.dotfiles/home/ssh/config $HOME/.ssh/config
