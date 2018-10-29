6919d66fefc2237edd1c978071aa8278808e4f6e
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-10-29 10:38:35 +0100

    byoby

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 grep -q dotfiles $HOME/.bashrc || echo "[ -r $HOME/.dotfiles/bash/bashrc ] && . $HOME/.dotfiles/bash/bashrc" >> $HOME/.bashrc
 
-ln -f -v -s $HOME/.dotfiles/home/gitignore $HOME/.gitignore
-ln -f -v -s $HOME/.dotfiles/home/gitconfig $HOME/.gitconfig
-ln -f -v -s $HOME/.dotfiles/home/emacs $HOME/.emacs
-ln -f -v -s $HOME/.dotfiles/home/ssh/config $HOME/.ssh/config
+ln -sfv $HOME/.dotfiles/home/gitignore $HOME/.gitignore
+ln -sfv $HOME/.dotfiles/home/gitconfig $HOME/.gitconfig
+ln -sfv $HOME/.dotfiles/home/emacs $HOME/.emacs
+ln -sfv $HOME/.dotfiles/home/ssh/config $HOME/.ssh/config
+test -d $HOME/.byobu && mv $HOME/.byobu /tmp ; ln -sfv $HOME/.dotfiles/home/byobu $HOME/.byobu
 
 . $HOME/.bashrc
